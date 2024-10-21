5aa64061726856314e1b649aae28a283ea050ce1
jose <josesaa0324@gmail.com>
2024-10-20 21:39:09 -0500

    commi inicial

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let p1 = parseInt(prompt
+    ("periodo 1"))
+    
+let p2 = parseInt(prompt
+        ("periodo 2")) 
+
+let p3 = parseInt(prompt
+            ("periodo 3"))      
+    
+let promedio = (p1+p2+p3)/3
+      
+    //mostrar resultado//
+    
+    console.log("el promedio es", promedio)
+    
+    
